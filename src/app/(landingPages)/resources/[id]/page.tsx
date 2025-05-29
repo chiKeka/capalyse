efb6d34bb0040b5e14f04a63d11309d97b346846
matthew <ekumamatthew1@gmail.com>
2025-05-29 21:59:02 +0100

    contact page, resource and resources deatil spage

---
+++ src/app/(landingPages)/resources/[id]/page.tsx
@@ -0,0 +1,60 @@
+import ResourceCard from "@/components/sections/ResourceCard";
+import { resouresData } from "@/lib/uitils/contentData";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+
+export async function generateStaticParams() {
+  return resouresData.map((r) => ({ id: r.id }));
+}
+const getRandomThree = (arr: typeof resouresData) => {
+  return [...arr].sort(() => 0.5 - Math.random()).slice(0, 3);
+};
+
+export default function ResourceDetailsPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const resource = resouresData.find((r) => r.id === params.id);
+
+  if (!resource) return notFound();
+
+  const randomThree = getRandomThree(resouresData);
+  return (
+    <div className="max-w-7xl mx-auto py-20 px-4">
+      <img
+        src={resource.image}
+        alt={resource.header}
+        className="w-full h-auto"
+      />
+      <div className="w-full items-center justify-center flex flex-col">
+        <h1 className="text-[56px] font-bold mb-2 text-center">
+          {resource.header}
+        </h1>
+        <div className="flex items-center justify-center gap-2">
+          <p className="mt-4 text-center">May 19, 2025 </p>
+          <p className="mt-4 text-center">6 minutes read</p>
+        </div>
+      </div>
+
+      <div>
+        <p className="mt-4 text-center">{resource.text}</p>
+      </div>
+
+      <div className="w-full mt-12 flex flex-col">
+        <p className="font-bold text-4xl  text-[#282828]">Other Resources </p>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-3">
+          {randomThree.map((item, index) => (
+            <Link href={`/resources/${item.id}`} key={index}>
+              <ResourceCard
+                header={item.header}
+                text={item.text}
+                image={item.image}
+              />
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/sections/Contact.tsx
@@ -0,0 +1,63 @@
+import Button from "../ui/Button";
+import Input from "../ui/Inputs";
+
+type Props = {};
+
+function Contact({}: Props) {
+  return (
+    <section className=" py-20 relative">
+      <div className="max-w-7xl lg:flex-row flex flex-col  justify-between mx-auto px-4 sm:px-6 lg:px-8 relative">
+        <div className="w-full max-w-md mb-8">
+          <h1 className="text-5xl lg:text-6xl font-bold leading-tight mb-6">
+            Contact <span className="text-green"> our team</span>
+            <br />
+          </h1>
+
+          <p>
+            Got any questions or concerns about any of our services? We really
+            can’t wait to hear from you!
+          </p>
+        </div>
+        <div className="w-full lg:max-w-xl">
+          <Input
+            name="name"
+            onChange={() => {}}
+            type="text"
+            value=""
+            label="Full Name"
+            placeholder="John Doe"
+          />
+          <Input
+            name="email"
+            onChange={() => {}}
+            type="email"
+            value=""
+            label="Email"
+            placeholder="You@company.com"
+          />
+          <Input
+            name="phone"
+            onChange={() => {}}
+            type="phone"
+            value=""
+            label="Phone Number"
+            placeholder="2340000000"
+          />
+          <Input
+            name="message"
+            onChange={() => {}}
+            type="textarea"
+            value=""
+            label="Message"
+            placeholder="Leave us a message"
+          />
+          <div className="w-full justify-end items-end flex">
+            <Button>Send Message</Button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Contact;
