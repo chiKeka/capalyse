390034fd964241f5f08833bf0af0ab1ae6b9f2ae
matthew <ekumamatthew1@gmail.com>
2025-06-06 06:12:19 +0100

    auth screen

---
+++ src/app/(auth)/reset_password/page.tsx
@@ -0,0 +1,54 @@
+"use client";
+import AuthLayout from "@/components/layout/auth";
+import Button from "@/components/ui/Button";
+import Input from "@/components/ui/Inputs";
+import Link from "next/link";
+
+type Props = {};
+
+function page({}: Props) {
+  return (
+    <>
+      <AuthLayout
+        sub_caption="Your new password should be easy to recall"
+        google_signtures={false}
+        title="Reset your password"
+      >
+        <div className="w-full">
+          <Input
+            name="new_password"
+            onChange={() => null}
+            type="password"
+            label="Enter new password"
+            className="h-[43px]"
+            placeholder="**********"
+            value=""
+          />
+
+          <Input
+            name="confirm_password"
+            onChange={() => null}
+            className="h-[43px]"
+            type="password"
+            label="Confirm password"
+            placeholder="**********"
+            value=""
+          />
+          <Button size="medium" variant="primary" className="font-bold w-full">
+            Reset Password
+          </Button>
+          <div className="flex flex-col items-center justify-center my-6">
+            <Link
+              href={"/login"}
+              className="flex font-bold text-green text-sm text-center items-center"
+            >
+              Return to login page
+            </Link>
+          </div>
+        </div>
+      </AuthLayout>
+    </>
+  );
+}
+
+export default page;
---
+++ src/components/layout/auth.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+interface AuthLayoutProps {
+  title?: string;
+  children: React.ReactNode;
+  google_signtures?: boolean;
+  sub_caption?: string;
+}
+
+const AuthLayout: React.FC<AuthLayoutProps> = ({
+  title,
+  children,
+  google_signtures,
+  sub_caption,
+}) => {
+  return (
+    <div className="min-h-screen flex flex-col w-full items-center justify-center bg-[#EEF6F4]  px-4">
+      <div className=" w-full px-4 h-auto lg:max-w-2xl bg-white flex-col flex gap-8 rounded-4xl border border-[#EEF6F4]  items-center  py-10  shadow-lg">
+        {/* <img src={}/> */}
+        <h2 className="font-bold text-3xl text-[#282828] text-center w-full">
+          Capalyse
+        </h2>
+        <div>
+          <h2 className="text-xl font-bold text-[#2E3034]  text-center">
+            {title}
+          </h2>
+          {sub_caption && (
+            <p className="text-[#8A8A8A] text-center text-sm mt-2 font-normal">
+              {sub_caption}
+            </p>
+          )}
+        </div>
+
+        {google_signtures && (
+          <>
+            <button className="max-w-md w-full rounded-lg  py-3 font-medium text-sm text-[#2E3034] items-center flex border-[0.5] border-[#829AD9] justify-center">
+              <img /> Sign up with Google
+            </button>
+            <div className="flex w-full max-w-md gap-2 items-center justify-center ">
+              <hr className="h-[0.5px] w-full bg-[#1261AC]" /> or
+              <hr className="h-[0.5px] w-full bg-[#1261AC]" />
+            </div>
+          </>
+        )}
+
+        <div className="w-full max-w-md flex-col items-center justify-center flex">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AuthLayout;
