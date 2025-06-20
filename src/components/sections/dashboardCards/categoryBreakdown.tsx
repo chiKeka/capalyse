b1af5726d062fb4902357d497f68b4cd15f8b129
matthew <ekumamatthew1@gmail.com>
2025-06-20 18:39:37 +0100

    fix for  mobile

--- src/components/sections/dashboardCards/categoryBreakdown.tsx
+++ src/components/sections/dashboardCards/categoryBreakdown.tsx
@@ -5,7 +5,7 @@ type Props = { caption?: string; label?: string; value?: number };
 export default function CategoryBreakdown({ caption, label, value }: Props) {
   return (
     <div className="w-full gap-2 py-2 flex flex-col">
-      <div className="w-full min-w-[750px] flex flex-row justify-between items-start">
+      <div className="w-full lg:min-w-[750px] flex flex-row justify-between items-start">
         <p className="text-base font-normal text-[#0B0B0C]">{caption}</p>
         <div className="flex flex-col gap-3 items-end justify-end text-end">
           <p className="text-[#0B0B0C] font-bold text-xs">{value}% Ready</p>
