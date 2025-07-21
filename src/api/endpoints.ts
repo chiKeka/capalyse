e253811174a3b90ed7e6a6d22c748ffee26bb712
matthew <ekumamatthew1@gmail.com>
2025-07-21 13:50:38 +0100

    Refactor API endpoint definitions for consistency: Standardize string quotes and add new investment interest endpoints. Comment out window reload in readiness form for improved functionality.

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -1,37 +1,37 @@
 export const ApiEndPoints = {
   // Wait list
-  Waitlist_Count: '/waitlist',
-  wait_list: '/waitlist',
+  Waitlist_Count: "/waitlist",
+  wait_list: "/waitlist",
   Get_Waitlist: (email: string) => `waitlist/status/${email}`,
 
   //Resources
 
-  Resources: '/resources',
+  Resources: "/resources",
   Single_Resource: (id: string) => `/resources/${id}`,
   Resource_Category: (category: any) => `/resources/category/${category}`,
-  Search_Resources: '/resources/search',
-  Popular_Resources: '/resources/popular',
+  Search_Resources: "/resources/search",
+  Popular_Resources: "/resources/popular",
 
   //Authentication
   Auth_Activity: (action: string) => `/auth/${action}`, // this actions is either {login, google, refresh, me, logout, verify-email, forgot-password, reset-password, token, register etc}
   Register_Activity: (action: string) => `/auth/register/${action}`, // this action is either (initiate, personal-info, dev-org-info, sma-business-info, investor-investment-info)
 
   //Assesment
-  Assessments: '/assessments',
+  Assessments: "/assessments",
   Single_Assessment: (id: string) => `assessments/${id}`,
   Assessment_progress: (id: string) => `assessments/${id}/progress`,
   Submit_Assessment: (id: string) => `assessments/${id}/submit`,
 
   //files
 
-  Upload_File: '/files/upload',
-  Files: '/files',
+  Upload_File: "/files/upload",
+  Files: "/files",
   Single_Files: (id: string) => `/files/{id}`,
 
   // profile management
-  Profile: '/me',
-  Profile_Completion: '/me/profile-completion',
-  Profile_Info: '/me/personal-info',
+  Profile: "/me",
+  Profile_Completion: "/me/profile-completion",
+  Profile_Info: "/me/personal-info",
 
   //SMEs
   SMEs_Profile: (action?: string) => `/smes/me/${action}`,
@@ -41,11 +41,11 @@ export const ApiEndPoints = {
 
   //investor
 
-  Investor_Investments: '/investors/me/investment-info',
-  Investor_Organisation: '/investors/me/organization-info',
+  Investor_Investments: "/investors/me/investment-info",
+  Investor_Organisation: "/investors/me/organization-info",
 
   // development
-  Dev_Org: '/dev-orgs/me/organisation-info',
+  Dev_Org: "/dev-orgs/me/organisation-info",
 
   // invest readiness scoring
 
@@ -55,4 +55,10 @@ export const ApiEndPoints = {
 
   Administration_Config: `/scoring/admin/config`,
   Administration_Queue: `/scoring/admin/queue`,
+
+  //investmenst interest for investor matches
+
+  Investment_interest: (action?: string) => `/investments/interest/${action}`,
+  Request_due_delegene: (id: string) =>
+    `/investments/interests/${id}/due-diligence`,
 };
